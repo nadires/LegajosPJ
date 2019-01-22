e81d82f6d865d312d590853f81a7f54e5a99c5f4
Maximaube <maximaube@gmail.com>
2019-01-22 11:27:57 -0300

    implementado dataTable.js para filtrado dinámico de tabla

---
+++ legajos_project/assets/js/custom_datatable.js
@@ -0,0 +1,20 @@
+$(document).ready( function () {
+    $('#listTable').DataTable({
+        paging: false,      // Oculto la paginacion
+        "bInfo" : false,     // Oculto el "Showing 1 of N entries"
+        "language": {
+          "emptyTable": "No hay datos cargados",
+          "zeroRecords": "No hay resultados en la búsqueda"
+        }
+    });
+
+    // Defino que el input search es el creado manualmente
+    var oTable = $('#listTable').DataTable();
+    $("#input_search").on("keyup search input paste cut", function() {
+        oTable.search($(this).val()).draw() ;
+    });
+
+    // Oculto el input search que viene por defecto
+    $('#listTable_filter').parent().parent().hide();
+
+} );
