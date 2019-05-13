4d8b66568b06f839b21ea0111fbb600ae566ef4e
Maximaube <maximaube@gmail.com>
2019-05-13 11:23:31 -0300

    js para selección dinámica de dependencias

---
+++ legajos_project/assets/js/buscar_dependencia.js
@@ -0,0 +1,31 @@
+$(document).ready( function () {
+    $('.btnSeleccionarDependencia').on("click", function() {
+        var tipo = $(this).parents('td').siblings('.fila_tipo').html();
+        var codigo = $(this).parents('td').siblings('.fila_codigo').html();
+        var getUrl = window.location;
+        var datos = {tipo: tipo, cod: codigo};
+        $.ajax({
+                method: 'POST',
+                url: getUrl.origin+'/empleado/api/v1/dependencias',
+                data: datos,
+                success: function(data, status, xho){
+                    cargarSelects(data.data);
+                    $('#modalBuscarDependencias').modal('hide');
+                },
+                error: function(data){
+                    console.log(data);
+                }
+        })
+    });
+
+    function cargarSelects(resultado){
+        $('#id_circunscripcion').val(resultado['id_circunscripcion']);
+        $('#id_unidad').val(resultado['id_unidad']);
+        $('#id_organismo').val(resultado['id_organismo']);
+        $('#id_dependencia').val(resultado['id_dependencia']);
+        $('#id_direccion').val(resultado['id_direccion']);
+        $('#id_departamento').val(resultado['id_departamento']);
+        $('#id_division').val(resultado['id_division']);
+    }
+
+} );
