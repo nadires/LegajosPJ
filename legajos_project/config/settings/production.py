da04ba87b8789af3bff2dcfb4f311ba141afcdf9
Maximaube <maximaube@gmail.com>
2019-06-10 11:51:21 -0300

    Cambiada configuracion para que tome production.py

--- legajos_project/config/settings/production.py
+++ legajos_project/config/settings/production.py
@@ -1,7 +1,7 @@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
@@ -10,9 +10,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'SistemaLegajos',
-        'USER': 'postgres',
-        'PASSWORD': '3578563',
-        'HOST': 'localhost',
+        'USER': 'admin',
+        'PASSWORD': 'infoper123asd',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
