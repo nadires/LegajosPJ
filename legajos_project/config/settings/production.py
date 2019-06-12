e315f58b1d42ec86cfdf647d57ddb4692c3e97c2
Maximaube <maximaube@gmail.com>
2019-06-12 07:57:30 -0300

    Configurado production.py desde el server para dejarlo en funcionamiento

--- legajos_project/config/settings/production.py
+++ legajos_project/config/settings/production.py
@@ -1,18 +1,17 @@
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '172.16.28.200']
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sistemalegajos',
         'USER': 'admin',
         'PASSWORD': 'infoper123asd',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
