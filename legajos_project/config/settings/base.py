23dfb3cd47e71d097127157bfe5e71778de7dd06
Maximaube <maximaube@gmail.com>
2019-06-12 10:53:20 -0300

    Cambiado setting a production.py

--- legajos_project/config/settings/base.py
+++ legajos_project/config/settings/base.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from .dev import *
+from .production import *
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
